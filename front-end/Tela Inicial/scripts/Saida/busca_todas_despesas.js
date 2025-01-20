a1442008728bdfd865e5f3ffcc54952f854b2417
Leandro <Leandro@leandro.com>
2025-01-20 18:39:23 -0300

    Criado a tela de despesas

--- front-end/Tela Inicial/scripts/Saida/busca_todas_despesas.js
+++ front-end/Tela Inicial/scripts/Saida/busca_todas_despesas.js
@@ -16,3 +16,52 @@ async function buscaDadosSaidas() {
     ExibeError(err.message);
   }
 }
+
+async function carregarDados() {
+  const saidas = await buscaDadosSaidas();
+
+  const todosDados = [...saidas];
+
+  todosDados.sort((a, b) => new Date(a.dtcadastro) - new Date(b.dtcadastro));
+
+  retornoDadosLista(todosDados);
+}
+
+async function retornoDadosLista(data) {
+  const tableBody = document.querySelector('#dataTable tbody');
+  tableBody.innerHTML = '';
+
+  data.forEach((item) => {
+    const row = document.createElement('tr');
+    row.dataset.id = item.id;
+
+    row.classList.add('linered');
+
+    row.innerHTML = `
+      <td>${item.tipo}</td>
+      <td>${item.descricao}</td>
+      <td>${item.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+      <td>${item.meioPagamento}</td>
+      <td>${new Date(item.dtcadastro).toLocaleString('pt-BR')}</td>
+    `;
+
+    tableBody.appendChild(row);
+  });
+}
+
+window.onload = function () {
+
+  const isValid = isTokenValid();
+
+  if (isValid === null) {
+    window.location.href = '../Login/index.html'
+  } else if (!isValid) {
+    logoutUser();
+  } else {
+    localStorage.removeItem('selectedRowId');
+    carregarDados();
+    somaDespesas();
+    setupAutoLogout();
+  }
+
+};
